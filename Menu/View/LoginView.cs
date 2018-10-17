29f0dc1a1eb109b420270b8ae55a60520fae43ef
Praveena <kpraveena491@gmail.com>
2018-10-17 12:40:02 +0200

    modified code

--- Menu/View/LoginView.cs
+++ Menu/View/LoginView.cs
@@ -25,46 +25,30 @@ namespace Menu.View
             string username = Console.ReadLine();
 
             Console.Write("Password: ");
-            string password = Console.ReadLine();
+            string password = Console.ReadLine();           
 
-            Console.WriteLine("\nIs this correct? (Y)es (N)o");
-            string keyInfo = Console.ReadLine();
-                                   
-            if (keyInfo=="Y" && keyInfo=="y")
+            var authenticateService = new AuthenticateService();
+
+            var user = authenticateService.Authenticate(_users, username, password);
+
+            if (user != null)
             {
-                var authenticateService = new AuthenticateService();
-                var user = authenticateService.Authenticate(_users, username, password);
-                if (user != null)
-                {
-                    Console.WriteLine("Successfully logged in!");
-                    Thread.Sleep(2000);
-                    Console.Clear();
-                    Console.WriteLine($"Role{user.Role}");
-                    return user;
-                }
-                else
-                {
-                    Console.WriteLine("Access Denied!");
-                    Thread.Sleep(2000);
-                    Console.Clear();
-                    Display();
-                }
-            }
-            else if(keyInfo=="N")
-            {
+                Console.WriteLine("Successfully logged in!");
+                Thread.Sleep(2000);
                 Console.Clear();
-                Console.WriteLine("Enter Valid Credentials");
-                Display();
+                Console.WriteLine($"Role: {user.Role}");
+                return user;
             }
             else
             {
+                Console.WriteLine("Access Denied!");
+                Thread.Sleep(2000);
                 Console.Clear();
-                Console.WriteLine("Wrong input please try again!");
                 Display();
-                
             }
-
+            
             Console.ReadKey();
+            return user;
             
         }
        
